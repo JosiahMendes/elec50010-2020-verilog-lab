63604b2f5508ee1afc25068af6e08139a9fadb72
jam419 <jam419@ic.ac.uk>
2020-10-27 22:38:00 +0000

    Week0 Tasks complete

--- 0-tools/and_not_testbench.v
+++ 0-tools/and_not_testbench.v
@@ -6,29 +6,42 @@ module and_not_testbench();
 
     initial begin
         /* INSERT WAVEFORM COMMANDS */
+        $dumpfile("and_not_testbench_waves.vcd");
+        $dumpvars(0,and_not_testbench);
 
         a=0;
         b=0;
         #1; // Delay one timeunit
         /* INSERT $display HERE */
         /* INSERT assert HERE */
-        $display("a=%d, b=%d, r=%d", a, b, d);
+        if ( d != ~(a&b) ) begin
+            $display("Error : a=%d, b=%d, d=%d,  expected=%d", a, b, d, ~(a&b));
+        end
         assert(d==1);
         
         a=0;
         b=1;
         #1; // Delay one timeunit
         assert(d==1);
+        if ( d != ~(a&b) ) begin
+            $display("Error : a=%d, b=%d, d=%d,  expected=%d", a, b, d, ~(a&b));
+        end
 
         a=1;
         b=0;
         #1; // Delay one timeunit
         assert(d==1);
+        if ( d != ~(a&b) ) begin
+            $display("Error : a=%d, b=%d, d=%d,  expected=%d", a, b, d, ~(a&b));
+        end
 
         a=1;
         b=1;
         #1; // Delay one timeunit
         assert(d==0);
+        if ( d != ~(a&b) ) begin
+            $display("Error : a=%d, b=%d, d=%d,  expected=%d", a, b, d, ~(a&b));
+        end
     end
 
     and_gate n1(.a(a), .b(b), .r(c));
